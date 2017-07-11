96dea383d93d828b798cf223362678f9efafa5b9
canatra <joy.bushnell@gmail.com>
2017-07-11 15:52:55 -0400

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,2 +1,23 @@
-<!DOCTYPE HTML>
-<!-- Functions.php will be used to hold the functions that will be called from other scripts: connect, insert,delete, search-->
+<?php 
+//Functions.php will be used to hold the functions that will be called from other scripts: connect, insert,delete, search-->
+
+function connection(){
+  $username='root';
+	$password=$_POST['password'];
+	$localhost = 'localhost';
+	$db = 'Databases_Project';
+	$conn = mysqli_connect($localhost, $username, $password);
+	if($conn) { echo 'Connected <BR>';}
+	else{exit("Unable to connect to $db");}
+	$db_selected = mysqli_select_db($conn, $db);
+	if($db_selected) {echo 'Database selected'.'<BR>';}
+	else { echo 'DB not selected'.mysqli_error().'<BR>';}
+	
+  
+}//function connection() 
+
+
+
+
+
+?>
