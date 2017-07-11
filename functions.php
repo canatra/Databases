f025817596b464d22cdea5ff79be6748a89cd480
canatra <joy.bushnell@gmail.com>
2017-07-11 15:53:49 -0400

    Update functions.php

--- functions.php
+++ functions.php
@@ -8,10 +8,10 @@ function connection(){
 	$db = 'Databases_Project';
 	$conn = mysqli_connect($localhost, $username, $password);
 	if($conn) { echo 'Connected <BR>';}
-	else{exit("Unable to connect to $db");}
+	else{ exit("Unable to connect to $db");}
 	$db_selected = mysqli_select_db($conn, $db);
 	if($db_selected) {echo 'Database selected'.'<BR>';}
-	else { echo 'DB not selected'.mysqli_error().'<BR>';}
+	else { exit("$db not selected");}
 	
   
 }//function connection() 
