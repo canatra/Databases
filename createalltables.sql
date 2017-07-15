c76e6735cd3555ab6315a022889a628c20231b99
canatra <joy.bushnell@gmail.com>
2017-07-15 14:27:00 -0400

    Add files via upload

--- createalltables.sql
+++ createalltables.sql
@@ -1,6 +1,6 @@
-CREATE TABLE product(pid int NOT NULL AUTO_INCREMENT, upc, productname, expdate_DATE, unit_cost DOUBLE, quantity int, min_threshold int, unit_price double, cid int, PRIMARY KEY(pid));
+CREATE TABLE product(pid int NOT NULL AUTO_INCREMENT, upc int, productname varchar(45), exp_date DATE, unit_cost DOUBLE, quantity int, min_threshold int, unit_price double, cid int, PRIMARY KEY(pid));
 
-CREATE TABLE makers(mid int NOT NULL AUTO_INCREMENT, manufacturer varchar(64), Address varchar(64), Phone char(16), Website  varchar(64), PRIMARY KEY(makerid));
+CREATE TABLE makers(mid int NOT NULL AUTO_INCREMENT, manufacturer varchar(64), Address varchar(64), Phone char(16), Website  varchar(64), PRIMARY KEY(mid));
 
 CREATE TABLE makes ( pid int NOT NULL AUTO_INCREMENT, mid   int, PRIMARY KEY(pid, mid));
 
@@ -11,7 +11,7 @@ CREATE TABLE Customer(Cardnum int NOT NULL AUTO_INCREMENT, customername varchar(
 CREATE TABLE Buy (tid int, pid int, quantitySold int, date DATE, storeID int, card_num int , PRIMARY KEY( tid));
 
 
-CREATE TABLE  Store (storeid INT NOT NULL AUTO_INCREMENT, email varchar(64), name varchar(64), location varchar(128), phone char(16), PRIMARY KEY storeid);
+CREATE TABLE  Store (storeid INT NOT NULL AUTO_INCREMENT, email varchar(64), name varchar(64), location varchar(128), phone char(16), PRIMARY KEY (storeid));
 
 CREATE TABLE Employee( emplid int NOT NULL AUTO_INCREMENT, employeename varchar(64), storeid int, job_type varchar(25), hoursperweek int, wages double, phone char(16), email varchar(64), SSN char(11), totalhours INT, PRIMARY KEY(emplid));
 
