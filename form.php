11f7e40398db0bec740989de178682384bc645d0
canatra <joy.bushnell@gmail.com>
2017-07-17 12:00:52 -0400

    Add files via upload
    Changed some names/titles of things

--- form.php
+++ form.php
@@ -1,197 +1,167 @@
- 
- <!-- TABLE SELECTION -->
+
+<!-- TABLE SELECTION -->
  <select id="tablemenu" name ="tablemenu" onchange = "showForm()" > </h3> 
             <option value="none"></option>
-			<option value="category">category</option>
-		   <option value="product"> product </option>
-            <option value="maker"> maker  </option>
-            <option value="customer"> customer </option>
-            <option value="buy"> buy </option>
-			<option value="employee"> employee </option>
-			<option value="store"> store </option>
-			<option value="carries"> carries </option>
+			<option value="category">Category</option>
+		   <option value="product"> Product </option>
+            <option value="maker"> Manufacturer  </option>
+            <option value="customer"> Customer </option>
+            <option value="buy"> Transaction </option>
+			<option value="employee"> Employee </option>
+			<option value="store"> Store </option>
+			<option value="carries"> Carries </option>
             </select>
 
-<!-- FORM PRODCUCT -->  
-<form name="product" id="product" style="display:none" action="" method="post" onSubmit="alert('insert in a new tuple!')";>
-<h2>form product</h2>
+<!-- FORM PRODUCT -->  
+<form name="product" id="product" style="display:none" action="" method="post";>
+<h2>Add a New Product</h2>
 <ul>
-<li>pid: <input type="text" name="pid"></li>
+<li>product name: <input type="text" name="pname"></li>
 </ul>
 <ul>
-<li>productname: <input type="text" ></li>
+<li>expiration date: <input type="date"  name="expdate" ></li>
 </ul>
 <ul>
-<li>exp_date: <input type="text" ></li>
+<li>cost per unit: <input type="text" name="cost"></li>
 </ul>
 <ul>
-<li>unitcost: <input type="text" ></li>
+<li>minimum quantity: <input type="text" name="threshold""></li>
 </ul>
 <ul>
-<li>min_threshold: <input type="text" ></li>
+<li>quantity: <input type="text" name="quantity"></li>
 </ul>
 <ul>
-<li>quantity: <input type="text" ></li>
+<li>price per unit: <input type="text" name="price"></li>
 </ul>
 <ul>
-<li>unitprice: <input type="text" ></li>
-</ul>
-<ul>
-<li>cid: <input type="text" ></li>
-</ul>
-<ul>
-<li>mid: <input type="text" ></li>
+<li>category: <input type="text" name="cat" ></li>
 </ul>
 <p><input type="submit" name="product_submit"/></p>
 </form>
 
 <!-- FORM CATEGORY -->  
-<form name="category" id="category" style="display:none" action="" method="post" onSubmit="alert('insert in a new tuple!')";>
-<h2>form category</h2>
+<form name="category" id="category" style="display:none" action="" method="post";>
+<h2>Add a New Category</h2>
 <ul>
-<li>cid: <input type="text" name="cid"></li>
+<li>categoryname: <input type="text" name="catname"></li>
 </ul>
 <ul>
-<li>categoryname: <input type="text" ></li>
+<li>aisle: <input type="text" name="aisle"></li>
 </ul>
 <ul>
-<li>image: <input type="text" ></li>
-</ul>
-<ul>
-<li>description: <input type="text" ></li>
+<li>description: <input type="text" name="description"></li>
 </ul>
 
 <p><input type="submit" name="category_submit"/></p>
 </form>
 
 <!-- FORM MAKER -->  
-<form name="maker" id="maker" style="display:none" action="" method="post" onSubmit="alert('insert in a new tuple!')";>
-<h2>form maker</h2>
+<form name="maker" id="maker" style="display:none" action="" method="post";>
+<h2>Add a New Manufacturer</h2>
 <ul>
-<li>mid: <input type="text" name="mid"></li>
+<li>manufacturer: <input type="text" name="maker"></li>
 </ul>
 <ul>
-<li>makername: <input type="text" ></li>
+<li>address: <input type="text" name="address"></li>
 </ul>
 <ul>
-<li>address: <input type="text" ></li>
+<li>phone: <input type="tel" name="phone"></li>
 </ul>
 <ul>
-<li>phone: <input type="text" ></li>
-</ul>
-<ul>
-<li>email: <input type="text" ></li>
+<li>website: <input type="url" name="website"></li>
 </ul>
 <p><input type="submit" name="maker_submit"/></p>
 </form>
 
 <!-- CUSTOMER FORM -->
-<form name="customer" id="customer" style="display:none" action="" method="post" onSubmit="alert('insert in a new tuple!')";>
-<h2>form customer </h2>
+<form name="customer" id="customer" style="display:none" action="" method="post";>
+<h2>Add a New Customer </h2>
 <ul>
-<li>Card_num: <input type="text" ></li>
+<li>customername: <input type="text" name="customername"></li>
 </ul>
 <ul>
-<li>customername: <input type="text" ></li>
+<li>email: <input type="email" name="email"></li>
 </ul>
 <ul>
-<li>email: <input type="text" ></li>
-</ul>
-<ul>
-<li>address: <input type="text" ></li>
+<li>address: <input type="text" name="address"></li>
 </ul>
 <p><input type="submit" name="customer_submit"/></p>
 </form>
 
 <!-- BUY FORM -->
-<form name="buy" id="buy" style="display:none" action="" method="post" onSubmit="alert('insert in a new tuple!')";>
-<h2>form buy</h2>
+<form name="Transaction" id="buy" style="display:none" action="" method="post";>
+<h2>Add a New Transaction</h2>
 <ul>
-<li>tid: <input type="text" ></li>
+<li>product: <input type="text" name="product"></li>
+</ul>    <!--search the product table first to get the pid -->
+<ul>
+<li>quantity Sold: <input type="text" name="quantity"></li>
 </ul>
 <ul>
-<li>pid: <input type="text" ></li>
-</ul>
+<li>store name: <input type="text" name="store"></li>
+</ul>      <!-- search the store table to get storeid -->
 <ul>
-<li>quantitySold: <input type="text" ></li>
-</ul>
-<ul>
-<li>date: <input type="text" ></li>
-</ul>
-<ul>
-<li>storeid: <input type="text" ></li>
-</ul>
-<ul>
-<li>card_num: <input type="text" ></li>
-</ul>
+<li>customer name: <input type="text" name="customer_name"></li>
+</ul>	     <!-- search the customer table for customer id -->
 <p><input type="submit" name="buy_submit"/></p>
 </form>
 
 <!-- EMPLOYEE FORM -->
-<form name="employee" id="employee" style="display:none" action="" method="post" onSubmit="alert('insert in a new tuple!')";>
-<h2>form employee</h2>
+<form name="employee" id="employee" style="display:none" action="" method="post" ;>
+<h2>Add a New Employee</h2>
 <ul>
-<li>employeeid: <input type="text" ></li>
+<li>employee name: <input type="text" name="ename"></li>
 </ul>
 <ul>
-<li>employeename: <input type="text" ></li>
+<li>store name: <input type="text" name="storename"></li>
+</ul>	  	
+<ul>
+<li>job type (hourly or salary): <input type="text" name="job_type"></li>
 </ul>
 <ul>
-<li>storeid: <input type="text" ></li>
+<li>hours per week: <input type="text" name="hoursperweek"></li>
 </ul>
 <ul>
-<li>job_type: <input type="text" ></li>
+<li>wages: <input type="text" name="wages"></li>
 </ul>
 <ul>
-<li>hoursperweek: <input type="text" ></li>
+<li>phone: <input type="tel" name="phone"></li>
 </ul>
 <ul>
-<li>wages: <input type="text" ></li>
+<li>email: <input type="email" name="email"></li>
 </ul>
 <ul>
-<li>phone: <input type="text" ></li>
-</ul>
-<ul>
-<li>email: <input type="text" ></li>
-</ul>
-<ul>
-<li>direct_deposit: <input type="text" ></li>
-</ul>
-<ul>
-<li>SSN: <input type="text" ></li>
+<li>SSN: <input type="text" name="ssn"></li>
 </ul>
 <p><input type="submit" name="employee_submit"/></p>
 </form>
 
 <!-- STORE FORM -->
-<form name="store" id="store" style="display:none" action="" method="post" onSubmit="alert('insert in a new tuple!')";>
-<h2>form store</h2>
+<form name="store" id="store" style="display:none" action="" method="post";>
+<h2>Add a New Store</h2>
 <ul>
-<li>storeid: <input type="text" ></li>
+<li>store name: <input type="text" name="store"></li>
 </ul>
 <ul>
-<li>email: <input type="text" ></li>
+<li>email: <input type="email" name="email"></li>
 </ul>
 <ul>
-<li>storename: <input type="text" ></li>
+<li>location: <input type="text" name="location"></li>
 </ul>
 <ul>
-<li>location: <input type="text" ></li>
-</ul>
-<ul>
-<li>phone: <input type="text" ></li>
+<li>phone: <input type="tel" name="phone"></li>
 </ul>
 <p><input type="submit" name="store_submit"/></p>
 </form>
 
 <!-- CARRIES FORM -->
-<form name="carries" id="carries" style="display:none" action="" method="post" onSubmit="alert('insert in a new tuple!')";>
+<form name="carries" id="carries" style="display:none" action="";>
 <h2>form carries</h2>
 <ul>
-<li>pid: <input type="text" ></li>
+<li>pid: <input type="text" name="pid"></li>
 </ul>
 <ul>
-<li>storeid: <input type="text" ></li>
+<li>store id: <input type="text" name="storeid"></li>
 </ul>
 <p><input type="submit" name="carries_submit"/></p>
 </form>
