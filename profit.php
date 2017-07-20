302be2d70a299a6b78d03bf619e883de82c8e9a3
sherrygan01 <sherrygan01@163.com>
2017-07-20 16:52:46 -0400

    1) add profit calc page

---
+++ profit.php
@@ -0,0 +1,74 @@
+<?php
+include_once('main.php');
+?>
+<html>
+
+<p style="font-size:25px; color:lightblue;">Advanced Search</p>
+
+<h3>Find Profit by <select id="findmenu" name ="findmenu" onchange = "advancedForm()"> </h3> 
+            <option value="none"></option>
+			<option value="profitbyall"> All </option>
+		     <option value="profitbyproduct"> product </option>
+			
+			</select>
+		<form id="alllist" name ="alllist" style="display:none" method="post">	
+	   
+			 <input type="submit" name="all_submit">
+		</form>	
+		
+		<form id="productlist" name ="productlist" style="display:none" method="post">	
+	    
+			 <input type="text"  name="prodcut_profit">
+			 <input type="submit" name="product_submit">
+		</form>	
+		
+	
+
+
+<script>
+function advancedForm(){
+	var selopt = document.getElementById("findmenu").value;
+if(selopt=="profitbyproduct"){
+	document.getElementById("productlist").style.display="inline-block";
+	document.getElementById("alllist").style.display="none";
+}
+
+if(selopt=="profitbyall"){
+	document.getElementById("alllist").style.display="inline-block";
+	document.getElementById("productlist").style.display="none";
+	
+	
+}
+}
+</script>
+
+<body>
+
+
+
+<table>
+<?php
+include ('advancedfunctions.php');
+
+
+
+
+if (isset($_POST['advanced_submit'])){
+
+
+	$option= isset($_POST['selection'])?$_POST['selection']:false;
+	
+	if ($option)
+	{
+	 
+	advancedSearch($option);
+	echo "<br><br><br>";	
+
+	}
+}
+
+?>
+</table>
+
+</body>
+</html>
